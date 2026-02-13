86a469e77e5d2eb95e117e63b1353e95b8fe577a
Joseph R <raczynski.joseph616@gmail.com>
2026-02-13 13:21:27 -0600

    Import Scanner and ApiConfig

--- src/main/java/com/example/practice_weather_dashboard_Joseph_R/PracticeWeatherDashboardJosephRApplication.java
+++ src/main/java/com/example/practice_weather_dashboard_Joseph_R/PracticeWeatherDashboardJosephRApplication.java
@@ -1,16 +1,35 @@
 package com.example.practice_weather_dashboard_Joseph_R;
 
+import com.example.practice_weather_dashboard_Joseph_R.config.ApiConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class PracticeWeatherDashboardJosephRApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(PracticeWeatherDashboardJosephRApplication.class, args);
 
         RestTemplate restTemplate = new RestTemplate();
+        String apiKey = ApiConfig.getApiKey();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n=== Morning Weather Dashboard ===");
+            System.out.println("1. New York");
+            System.out.println("2. St. Louis");
+            System.out.println("3. Chicago");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+
 	}
 
 }
