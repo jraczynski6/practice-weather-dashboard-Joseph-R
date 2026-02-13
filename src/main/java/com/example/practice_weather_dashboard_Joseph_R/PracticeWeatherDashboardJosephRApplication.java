85508dfa3ccbe2192ffbaa51519f3e297934a026
Joseph R <raczynski.joseph616@gmail.com>
2026-02-13 12:43:03 -0600

    Initial commit

---
+++ src/main/java/com/example/practice_weather_dashboard_Joseph_R/PracticeWeatherDashboardJosephRApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_weather_dashboard_Joseph_R;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeWeatherDashboardJosephRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeWeatherDashboardJosephRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practice_weather_dashboard_Joseph_R/PracticeWeatherDashboardJosephRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_weather_dashboard_Joseph_R;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeWeatherDashboardJosephRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
